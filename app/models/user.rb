a017650768fc325397769081e30cd9af000b919b
darkelv <darkelv777@yandex.ru>
2020-07-16 16:22:16 +0300

    add_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
 
   VALID_EMAIL_FORMAT = /.+@.+\..+/i
+
   ROLES = { user: 0, admin: 1, implementer: 2, moderator: 3, accountant: 4 }
-  
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -12,6 +15,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_FORMAT, message: "Неверный формат почта" }
 
   enum role: ROLES
-
 
 end
